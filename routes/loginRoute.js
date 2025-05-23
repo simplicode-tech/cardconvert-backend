65fedd83cfffb16c5074217f96b433e61a8e9e71
Simplicode <simplicode01@gmail.com>
2025-05-23 03:54:02 +0100

    ups

--- routes/loginRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import {login} from '../handlers/loginHandler.js';
-router.post('/loginpage', login);
-export default router;
--- routes/signUpRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { signup } from "../handlers/signUpHandler.js";
-router.post('/signuppage', signup);
-export default router;
