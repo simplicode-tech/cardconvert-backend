67465c9922090404528f5e01fea1ce2cadb86c50
Simplicode <simplicode01@gmail.com>
2025-05-24 19:16:37 +0100

    ups

--- backend/routes/tradegiftcardroute.js
+++ backend/routes/tradegiftcardroute.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { giftcardupload } from '../handlers/tradeGiftCardHandler.js';
 const router = express.Router();
-router.post('/uploadgiftcard', giftcardupload);
+import { giftcardupload } from '../handlers/tradeGiftCardHandler.js';
+import authenticateUser from '../middlewares/authenticateUser.js';
+router.post('/uploadgiftcard', authenticateUser, giftcardupload);
 export default router;
